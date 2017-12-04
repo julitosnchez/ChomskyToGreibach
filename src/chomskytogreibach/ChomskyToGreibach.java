013276ff0be3017ef2c0aea8ae7cc62441e2cb98
julitosnchez <julitosnchez@julitoSanchez>
2017-12-04 13:29:08 +0100

    "ELIMINA 2 NO AÑADIA Ba, SUSTITUIA Y ADEMAS IMPLEMENTADOS 2 ORDENES"

--- src/chomskytogreibach/ChomskyToGreibach.java
+++ src/chomskytogreibach/ChomskyToGreibach.java
@@ -22,21 +22,33 @@ public class ChomskyToGreibach {
         // TODO code application logic here
         GramaticaChomsky GC = new GramaticaChomsky();  
         
-        GC.añadeProduccion("C", "b");
-        GC.añadeProduccion("D", "a");
-        GC.añadeProduccion("C", "DA");
-        GC.añadeProduccion("A", "CD");
-        GC.añadeProduccion("D", "AC");
-//        GC.añadeProduccion("F", "b");
+        GC.añadeProduccion("A", "b");
+        GC.añadeProduccion("B", "c");
+        //GC.añadeProduccion("C", "DA");
+        GC.añadeProduccion("C", "e");
+        GC.añadeProduccion("D", "GB");
+        GC.añadeProduccion("E", "AH");
+        GC.añadeProduccion("F", "FD");
+        GC.añadeProduccion("F", "HE");
+        GC.añadeProduccion("F", "GC");
+        GC.añadeProduccion("G", "FF");
+        GC.añadeProduccion("G", "d");
+        GC.añadeProduccion("H", "GC");
+
 
  
-//        System.out.println(GC.toString());
-        GC.renombraVariables();
-        GC.ELIMINA1("3", "1");
-        GC.ELIMINA1("3", "2");
-        GC.ELIMINA2("3");
-        GC.ELIMINA1("2", "3");
-        GC.ELIMINA1("1", "2");
+        //ESPECIFICAMOS ORDEN
+        // 0 - Orden ALfabetico
+        // 1- Inversa Orden ALfabetico
+          GC.renombraVariables(0);
+//        GC.ELIMINA1("3", "1");
+//        GC.ELIMINA1("3", "2");
+//        GC.ELIMINA2("3");
+//        GC.ELIMINA1("2", "3");
+//        GC.ELIMINA1("1", "2");
+//        GC.ELIMINA1("-3", "1");
+//        GC.ELIMINA1("-3", "1");
+        GC.CNFtoGNF();
         System.out.println(GC.toString());
 //        GC.ELIMINA2("2");
 //        System.out.println(GC.toString());
